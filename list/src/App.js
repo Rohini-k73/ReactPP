479e72dabd0611d502fd5259fcf4f51cc357d03c
Rohini Kulkarni <kulkarnirohini879@gmail.comgit config --global user.emailgit config --global user.email kulkarnirohini879@gmail.com>
2023-04-09 00:27:38 +0530

    add
    add
    
    practice

--- list/src/App.js
+++ list/src/App.js
@@ -27,16 +27,12 @@ export default function App() {
           <Route path="/passingobjects" element={<PassingObjects />} />
           <Route path="/input" element={<InputStuff />} />
           <Route path="/slideshow" element={<ImageSlider />} />
-          {/* <Route path="about" element={<About />} /> */}
-          {/* <Route path="dashboard" element={<Dashboard />} /> */}
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
+        
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
       {/* <FooterContact /> */}
-      {/* <Ameesha /> */}
+      
     </div>
   );
 }
